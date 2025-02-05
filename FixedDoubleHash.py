2dfa42a06a402025454078604abe16e6ab9d7bea
charlie-curtis <charlie-curtis@outlook.com>
2025-02-04 19:08:50 -0500

    Update FixedDoubleHash.py

--- FixedDoubleHash.py
+++ FixedDoubleHash.py
@@ -7,6 +7,7 @@ class FixedDoubleHash:
         self.MOD1 = 10**9 + 7
         self.MOD2 = 10**9 + 9
 
+    #WHEN COLLAPSING WINDOW, DROP BEFORE YOU ADD
     def drop(self,i):
         self.h1 -= pow(26*31, self.m-1, self.MOD1)*ord(self.s[i])
         self.h2 -= pow(26*31, self.m-1, self.MOD2)*ord(self.s[i])
